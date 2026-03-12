643e46cdc07ca7cd406b0fd99fc702b2c5182f10
Akshat Malviya <ancientfuture1337@gmail.com>
2026-03-12 21:02:31 +0900

    Add comment.lua package and update formatting in autopairs and telescope.

--- nvim/lua/plugins/autopairs.lua
+++ nvim/lua/plugins/autopairs.lua
@@ -1,7 +1,11 @@
 return {
-  'windwp/nvim-autopairs',
-  event = "InsertEnter",
-  config = true
-  -- use opts = {} for passing setup options
-  -- this is equivalent to setup({}) function
+  {
+
+    'windwp/nvim-autopairs',
+    event = "InsertEnter",
+    config = true,
+    opts = {}
+    -- use opts = {} for passing setup options
+    -- this is equivalent to setup({}) function
+  }
 }
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,35 +1,37 @@
 return {
-  'nvim-telescope/telescope.nvim',
-  version = '*',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    -- optional but recommended
-    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-  },
-  config = function()
-    require('telescope').setup {
-      pickers = {
-        find_files = {
-          theme = "ivy"
+  {
+    'nvim-telescope/telescope.nvim',
+    version = '*',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      -- optional but recommended
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    },
+    config = function()
+      require('telescope').setup {
+        pickers = {
+          find_files = {
+            theme = "ivy"
+          }
+        },
+        extensions = {
+          fzf = {}
         }
-      },
-      extensions = {
-        fzf = {}
       }
-    }
 
-    require('telescope').load_extension('fzf')
+      require('telescope').load_extension('fzf')
 
-    vim.keymap.set("n", "<space>fd", require('telescope.builtin').find_files)
-    vim.keymap.set("n", "<space>en", function()
-      require('telescope.builtin').find_files {
-        cwd = vim.fn.stdpath("config")
-      }
-    end)
-    vim.keymap.set("n", "<space>ep", function()
-      require('telescope.builtin').find_files {
-        cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy")
-      }
-    end)
-  end
+      vim.keymap.set("n", "<space>fd", require('telescope.builtin').find_files)
+      vim.keymap.set("n", "<space>en", function()
+        require('telescope.builtin').find_files {
+          cwd = vim.fn.stdpath("config")
+        }
+      end)
+      vim.keymap.set("n", "<space>ep", function()
+        require('telescope.builtin').find_files {
+          cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy")
+        }
+      end)
+    end
+  }
 }
