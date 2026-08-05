5f01ea7181364f7f94e1e8be3a8d676af96ac8a4
Akshat Malviya <ancientfuture1337@gmail.com>
2026-08-05 13:28:33 +0900

    Add keybind for screenshot using quickshell IPC

--- hypr/modules/keybinds.lua
+++ hypr/modules/keybinds.lua
@@ -55,13 +55,14 @@ hl.bind("XF86AudioMicMute", hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SOURC
 hl.bind("XF86MonBrightnessUp", hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%+"), { locked = true, repeating = true })
 hl.bind("XF86MonBrightnessDown", hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%-"), { locked = true, repeating = true })
 
--- hl.bind("PRINT",)
-
 -- Requires playerctl
 hl.bind("XF86AudioNext", hl.dsp.exec_cmd("playerctl next"), { locked = true })
 hl.bind("XF86AudioPause", hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
 hl.bind("XF86AudioPlay", hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
 hl.bind("XF86AudioPrev", hl.dsp.exec_cmd("playerctl previous"), { locked = true })
+
+-- Requires spades quickshell
+hl.bind("PRINT", hl.dsp.exec_cmd("qs ipc call screenshotOverlay toggle"))
 
 -- Submaps --
 hl.define_submap("powermenu_submap", function()
